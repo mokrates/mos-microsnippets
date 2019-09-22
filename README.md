99eb70d4835e7b08e1c7c26b861e3a2001312551
Moritz Molle <mmolle@users.sourceforge.net>
2019-09-22 10:48:17 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # mos-microsnippets
 a little something like yasnippet but much simpler
+
+## Usage
+  * Mark your region you want to use as snippet
+  * C-= w to copy the region (C-u C-= w to set new jump marker and
+    copy the region)
+  * C-= y to yank (insert) the region
+  * You have to bind this keys to the corresponding functions
+    yourself.  I like this bindings, you can, of course, use your own.
+    (see source)
+  
+### Syntax
+  There is something I call jump marker, because the insert function
+  jumps there.  The default is '@' but you can customize it to
+  something different.  Also: C-u C-= w asks for a new jump marker if
+  '@' should not be suitable to your snippet.
+  
+  If you insert "foo @(bar) baz", you will be asked to fill in the
+  "bar" blank.
+  
+  Inserting "@[2] spam and @[3] bacon" yields for the first insert:
+  
+	  2 spam and 3 bacon
+  and for the second insert:
+  
+	  3 spam and 4 bacon
+  and so on.
+  
+  Copying a new region resets the incrementation counter to zero.
+  
+  
+  
---
+++ mo_microsnippet.el
@@ -0,0 +1,98 @@
+;; Mo's MicroSnippet
+
+;; GPLv2 or later
+;; (c) 2019 Moritz Molle
+
+;;;;;;;;;;;;; Insert this into your .emacs
+
+;;;;;;;; mo microsnippets
+;; (load "~/.emacs.d/mo_microsnippet.el")  ;; to load this file (adjust the path if needed)
+;; (global-set-key (kbd "C-= w") 'mms-copy-region)     ;; I like C-= as prefix key
+;; (global-set-key (kbd "C-= y") 'mms-insert-snippet)
+;;;;;;;;;;;;;;;;;;;;
+
+;; create simple snippets
+;; @(...)-parts, you can jump to with [TAB]
+;; @[012]-parts will automatically counted up
+;; you can use $() or %() or whatever by changing "mms-jump-marker"
+
+
+(defcustom mms-jump-marker "@"
+  "This is the mms jump marker with which to mark fields in mms-snippets")
+(defvar mms-increment-counter 0
+  "the mms increment count. Gets zeroed on mms-copy-region")
+(defvar mms-the-snippet ""
+  "this is the mms snippet clipboard")
+
+;; stolen helper-functions
+;; source: https://www.emacswiki.org/emacs/IncrementNumber
+(defun mms--increment-number-decimal (&optional arg)
+  "Increment the number forward from point by 'arg'."
+  (interactive "p*")
+  (save-excursion
+    (save-match-data
+      (let (inc-by field-width answer)
+        (setq inc-by (if arg arg 1))
+        (skip-chars-backward "0123456789")
+        (when (re-search-forward "[0-9]+" nil t)
+          (setq field-width (- (match-end 0) (match-beginning 0)))
+          (setq answer (+ (string-to-number (match-string 0) 10) inc-by))
+          (when (< answer 0)
+            (setq answer (+ (expt 10 field-width) answer)))
+          (replace-match (format (concat "%0" (int-to-string field-width) "d")
+                                 answer)))))))
+
+(defun mms--count-marker ()
+  (let ((count-marker (concat mms-jump-marker "["))
+        (tmp mms-the-snippet)
+        (count 0))
+    (while (> (length tmp) (length count-marker))
+      (if (string-prefix-p count-marker tmp)
+          (incf count))
+      (setq tmp (substring tmp 1 nil)))
+    count))
+
+(defun mms-copy-region (p beg end)
+  "This function copies the region into the mms snippet clipboard (variable mms-the-snippet)
+If called with prefix argument asks for a new jump marker
+With '@' as jump-marker, @[123]-like fields will automatically incremented by mms-insert-snippet and
+@(foo) are fields which have to be filled in when pasting the snippet"
+  (interactive "P\nr")
+  (if p
+      (setq mms-jump-marker (read-from-minibuffer "Set new jumpmarker: " mms-jump-marker)))
+  (setq mms-the-snippet (buffer-substring-no-properties beg end))
+  (deactivate-mark)
+  (setq mms-increment-counter 0))
+
+(defun mms-insert-snippet ()
+  (interactive)
+  (let ((count-marker (concat mms-jump-marker "["))
+        (edit-marker (concat mms-jump-marker "("))
+        (p (point))
+        (end-marker nil)
+        (field-name))
+    (insert mms-the-snippet)
+    (setq end-marker (point-marker))
+    
+    ;; incrementing numbers
+    (goto-char p)
+    (dotimes (number (mms--count-marker) nil)
+      (search-forward count-marker)
+      (backward-delete-char (length count-marker))
+      (mms--increment-number-decimal mms-increment-counter)
+      (search-forward "]")
+      (backward-delete-char 1))
+    (incf mms-increment-counter)
+    
+    ;; editing edit-fields
+    (goto-char p)
+    (while (and (< (point) end-marker)
+                (search-forward edit-marker end-marker 'not-nil-and-not-t))
+      (backward-delete-char (length edit-marker))
+      (save-mark-and-excursion
+       (set-mark (point))
+       (search-forward ")")
+       (setq field-name (buffer-substring-no-properties (mark) (- (point) 1)))
+       (delete-region (mark) (point)))
+
+      (insert (read-from-minibuffer "Field: " field-name)))))
